190996ee44344f0deb6bfc4f5d6751ceb2fb27d8
Pedro Donaldo Sanchez Galvan <pd.sanchezgalvan@ugto.mx>
2022-01-08 20:29:34 -0600

    Final version of BMI calculator

--- BMIcalculator.py
+++ BMIcalculator.py
@@ -8,21 +8,21 @@
 Example for a BMI(Body mass index) calculator
 """
 def bmi_app():
-    height=input('How tall are you(cm)? ')
-    weight=input('How much do you weight(kg)? ')
-    bmi_val=int(weight)/(int(height)/100)**2
-    print('Your BMI is {}'.format(round(bmi_val,2)))
-    if (bmi_val < 18.5) :
-        print('You should better eat more :)')
-    elif bmi_val >= 18.5 and bmi_val <= 24:
-        print('Good job! Your BMI is OK')
-    else:
+    height=input('How tall are you(cm)? ')          #Ask for the weight
+    weight=input('How much do you weight(kg)? ')    #Ask for the height
+    bmi_val=int(weight)/(int(height)/100)**2        #Calulate the BM index
+    print('Your BMI is {}'.format(round(bmi_val,2)))#Show it with 2 decimals
+    if (bmi_val < 18.5) :                           #Compare if it is less than
+        print('You should better eat more :)')      #the recommended
+    elif bmi_val >= 18.5 and bmi_val <= 24:         #Or between the correct ones
+        print('Good job! Your BMI is OK')           
+    else:                                           #Or an bigger value
         print('You should better do some excercises :(')
 
 
 def main():
     
-    print("Hola Mundo")
+    print("Let\'s calculate your Body Mass Index ")
     bmi_app()
 
 
