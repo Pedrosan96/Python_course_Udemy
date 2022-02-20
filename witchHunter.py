bc5c2cceddf880de757396c570e56257e93b6eb3
Pedro Donaldo Sanchez Galvan <pd.sanchezgalvan@ugto.mx>
2022-02-19 23:16:22 -0600

    final, 3 diferentes brujas, opciones para derrotas y victorias

--- witchHunter.py
+++ witchHunter.py
@@ -7,7 +7,7 @@
 """
 Witch Hunter game implementing classes and inheritance
 """
-import random
+import random,time
 
 class Witch:
     def __init__(self, name, level):
@@ -19,7 +19,31 @@ class Witch:
 
     def witchcraft_attack(self):
         return random.randint(1,10)*self.level
-    
+
+class FireWitch(Witch):
+    def __init__(self, name, level, fire_staff):
+        super().__init__(name, level)
+        self.fire_staff=fire_staff
+
+    def witchcraft_attack(self):
+        base_attack=super().witchcraft_attack()
+        if self.fire_staff:
+            print('Witchcraft is filling with fire!!')
+            return base_attack * 2
+        else:
+            return base_attack
+
+class EvilWitch(Witch):
+    def __init__(self, name, level, black_staff_level):
+        super().__init__(name, level)
+        self.black_staff_level = black_staff_level
+
+    def witchcraft_attack(self):
+        base_attack=super().witchcraft_attack()
+        print(f'Witchcraft with black staff level{self.black_staff_level}!!')
+        return base_attack*2*self.black_staff_level
+        
+
 class Hunter:
     def __init__(self, name, level):
         self.name=name
@@ -28,9 +52,11 @@ class Hunter:
     def __repr__(self):
         return ('{} lv{}'.format(self.name, self.level))
 
-    def attack(self, witch):
-        hunter_damage = random.randint(1, 10) * self.level
+    def attack(self, witch, critical_strike):
+        base_damage = random.randint(1, 10) * self.level
+        hunter_damage=base_damage*2 if critical_strike else base_damage
         witch_damage=witch.witchcraft_attack()
+        critical_strike_msg = '(critical strike!)'if critical_strike else ''
         print('Hunter attacked {}'.format(hunter_damage))
         print('Witch attacked {}'.format(witch_damage))
         if hunter_damage > witch_damage:
@@ -40,15 +66,30 @@ class Hunter:
             print('Run away {}!'.format(self.name))
             return False
 
+def start_game():
+    hunter=Hunter('Leumas', 3)
 
+    while True:
+        witches=[Witch('Scarlett', random.randint(1,5)),
+                FireWitch('FireSabrina', random.randint(6,20), random.choice([True,False])),
+                EvilWitch('EvilCarlotta', random.randint(21,30), random.randint(1,3))]
+        witch=random.choice(witches)
+        print('\n{} has appeared!!\n'.format(witch.name))
+        cmd=input('Do you [a]ttack or [s]top tracing, [q]uit?')
+        if cmd=='a':
+            if hunter.attack(witch, random.choice([True, False])):
+                hunter.level +=1
+                print(f'Level up! {hunter.level} level now')
+            else:
+                print('Hunter is taking time to recover')
+                time.sleep(1)
+        elif cmd=='q':
+            break
+        else:
+            print('Hunter stopped tracing')
 
-def main():
-    witch = Witch('Scarlett', 5)
-    hunter = Hunter('Leumas', 3)
-
-    hunter.attack(witch)
 
 
 if __name__ == "__main__":
-    main()
+    start_game()
 
