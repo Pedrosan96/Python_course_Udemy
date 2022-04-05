76b329b4fc6886fc2babb37ef197897f336534fc
Pedro Donaldo Sanchez Galvan <pd.sanchezgalvan@ugto.mx>
2022-04-04 19:27:52 -0500

    Ejercicio completo para la suma de dos numeros del mismo muero de digitos

--- X.py
+++ X.py
@@ -19,15 +19,49 @@ def IntegerException(a):
 def PositiveAssertion(a):
     assert a > 0, "This is not a positive number"
 
-try:
-    IntegerException(var)
-except ValueError as e:
-    print(e)
+
+def MyDigitCounter(a):
+    '''
+    El objetivo es contar el numero de digitos en un número
+    '''
+    div = a/10
+    if div < 1:
+        return 1
+    elif div < 10:
+        return 2
+    else:
+        return 3
+
+
+def main():
+    #Primer requerimiento: entero positivo
+    val1=int(float(input('Dime el primer numero: ')))
+    try:
+        IntegerException(val1)
+    except ValueError as e:
+        print(e)
+
+    try:
+        PositiveAssertion(val1)
+    except AssertionError as e:
+        print(e)
+
+    val2=int(float(input('Dime el segundo numero: ')))
+    try:
+        IntegerException(val1)
+    except ValueError as e:
+        print(e)
     
-try:
-    PositiveAssertion(var)
-except AssertionError as e:
-    print(e)
+    try:
+        PositiveAssertion(val1)
+    except AssertionError as e:
+        print(e)
+    
+    ##Segundo requerimirnto: Numeros con el mismo numero de digitos
+    if MyDigitCounter(val1) == MyDigitCounter(val2):
+        return 'La suma es: ',  val1+val2
+    else:
+        return 'Numero de digitos diferente'
 
 
-
+print(main())
