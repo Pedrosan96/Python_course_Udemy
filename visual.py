3495b90faf20f3f88385f33841084d72da0e5ecb
Pedro Donaldo Sanchez Galvan <pd.sanchezgalvan@ugto.mx>
2022-04-05 20:35:21 -0500

    Se añade stack plot

--- visual.py
+++ visual.py
@@ -42,6 +42,51 @@ ax3.plot([-1,2,4,7,8],[5,2,3,4,3],'r')
 plt.show()
 
 
+data = {'Player': ['Wade','James','Kobe','Curry'],
+        'First': [10,10,8,12],
+        'Second': [12,8,13,8],
+        'Third': [15,12,8,8],
+        'Fourth': [18,20,15,8]}
+
+fig5 = plt.figure("Stacked bar")
+ax4=fig5.add_subplot(1,1,1)
+bar_width=0.5
+bars=[i+1 for i in range(len(data['First']))]
+ticks=[i + (bar_width/2) for i in bars]
+ax4.bar(bars,
+        data['First'],
+        width= bar_width,
+        label= 'First Quarter',
+        color='#AA5439')
+
+ax4.bar(bars,
+        data['Second'],
+        width= bar_width,
+        bottom= data['First'],
+        label= 'Second Quarter',
+        color='#AA5439')
+
+ax4.bar(bars,
+        data['Third'],
+        width= bar_width,
+        bottom= [i+j for i,j in zip(data['First'],data['Second'])],
+        label= 'Third Quarter',
+        color='#FF9200')
+
+ax4.bar(bars,
+        data['Fourth'],
+        width= bar_width,
+        bottom= [i+j+k for i, j, k in
+            zip(data['First'],data['Second'],data['Third'])],
+        label= 'First Quarter',
+        color='r')
+plt.xticks(ticks,data['Player'])
+ax4.set_xlabel('Total')
+ax4.set_ylabel('Player')
+plt.legend(loc='upper right')
+plt.xlim([min(ticks)-bar_width, max(ticks)+bar_width])
+plt.show()
+
 def main():
     """
     Comentario de la función
