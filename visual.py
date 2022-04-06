43faef5f2a07233051db49ea84cbedf743316c5e
Pedro Donaldo Sanchez Galvan <pd.sanchezgalvan@ugto.mx>
2022-04-05 20:51:34 -0500

    Se añade Scatter y Pie plot

--- visual.py
+++ visual.py
@@ -87,12 +87,19 @@ plt.legend(loc='upper right')
 plt.xlim([min(ticks)-bar_width, max(ticks)+bar_width])
 plt.show()
 
-def main():
-    """
-    Comentario de la función
-    """
+fig6=plt.figure("Scatter")
+ax5=fig6.add_subplot(1,1,1)
+ax5.scatter([-1,0,2,3,5],[2,1,3,0.5,4],[120,200,300,150,30],['r','g','#BCDFF0','#BB5500','b'])
+plt.show()
 
+fig7=plt.figure("Pie")
+sizes=[50,50,44,36]
+labels=['Wade','James','Kobe','Curry']
+explode=(0.1,0,0,0)
+colors=['red','purple','yellow','blue']
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%',
+        shadow=True, startangle=140)
+plt.axis('equal')
+plt.show()
 
-if __name__ == "__main__":
-    main()
 
